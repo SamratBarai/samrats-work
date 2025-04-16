b08f456c7c6a41b8556c721bc016a00cccd62de7
SamratBarai <baraisuranjit16@gmail.com>
2025-04-16 19:27:05 +0600

    Add volume controls

--- js/ringtone.js
+++ js/ringtone.js
@@ -1,5 +1,9 @@
 audios = document.querySelectorAll("audio");
+slider = document.getElementById("volume")
 
 audios.forEach(audio => {
     audio.volume = 0.1;
+    volume.addEventListener("input", function(){
+        audio.volume = slider.value / 1000
+    })
 });
